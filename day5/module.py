9eea37bf2a738f5c2e7e0ef28c234fa580c52628
clv <chenglv@live.com>
2017-07-10 23:37:49 +0800

    file operation

--- day5/module.py
+++ day5/module.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # LC
 
-
+'''
 # 导入模块是将模块中所有的代码打包成一个文件，通过.来调用
 import os
 # 导入包的本质是运行包中的__init__文件
@@ -28,21 +28,49 @@ y = time.mktime(x)       #将结构化数据转换成时间戳
 z = time.strftime("%Y-%m-%d %H:%M:%S",y) #将结构化数据转换成格式化数据
 #time.strftime("格式","结构化的时间数据（元组）")  --->将结构化时间数据转化成格式化时间数据
 #time.strptime("格式化时间字符串","格式")  ----->按着给定的格式进行匹配格式化时间字符串，并转换成格式化时间数据
-'''
->>> x = time.localtime()
->>> print(x)
-time.struct_time(tm_year=2017, tm_mon=7, tm_mday=9, tm_hour=23, tm_min=11, tm_se
-c=32, tm_wday=6, tm_yday=190, tm_isdst=0)
->>> time.strftime("%Y-%m-%d %H:%M:%S",x)        #即%Y去匹配tm_year,%m匹配tm_mon，无须注意顺序
-'2017-07-09 23:11:32'
->>>
->>> time.strptime('2017-07-09 23:11:32',"%Y-%m-%d %H:%M:%S")    %Y 匹配2017,必须注意顺序
-time.struct_time(tm_year=2017, tm_mon=7, tm_mday=9, tm_hour=23, tm_min=11, tm_se
-c=32, tm_wday=6, tm_yday=190, tm_isdst=-1)
 
-time.ctime(x)   #将时间戳数据转换成特定格式
->>> time.ctime(x)
-'Mon Jul 10 19:26:16 2017'
->>> time.asctime(y) #将结构化数据转换成特定格式
-'Mon Jul 10 19:26:45 2017'
+# >>> x = time.localtime()
+# >>> print(x)
+# time.struct_time(tm_year=2017, tm_mon=7, tm_mday=9, tm_hour=23, tm_min=11, tm_se
+# c=32, tm_wday=6, tm_yday=190, tm_isdst=0)
+# >>> time.strftime("%Y-%m-%d %H:%M:%S",x)        #即%Y去匹配tm_year,%m匹配tm_mon，无须注意顺序
+# '2017-07-09 23:11:32'
+# >>>
+# >>> time.strptime('2017-07-09 23:11:32',"%Y-%m-%d %H:%M:%S")    %Y 匹配2017,必须注意顺序
+# time.struct_time(tm_year=2017, tm_mon=7, tm_mday=9, tm_hour=23, tm_min=11, tm_se
+# c=32, tm_wday=6, tm_yday=190, tm_isdst=-1)
+#
+# time.ctime(x)   #将时间戳数据转换成特定格式
+# >>> time.ctime(x)
+# 'Mon Jul 10 19:26:16 2017'
+# >>> time.asctime(y) #将结构化数据转换成特定格式
+# 'Mon Jul 10 19:26:45 2017'
 '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day5/random_module.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# LC
+import random
+print(random.randint(1,9))
+print(random.random())
+a = 1
