e1abb13ae2f9267f18e740e18705979802df7180
clv89 <chenglv@live.com>
2017-07-20 20:24:03 +0800

    20170630

--- day5/calc.py
+++ day5/calc.py
@@ -11,49 +11,51 @@ def mul_div(str1):
     str1 = re.sub(r"\s","",str1)
     while not exit_flag:
             if "*" in str1:
-                equation = re.search(r"([\d|.]+\*[\d|.]+)",str1).group()
+                equation = re.search(r"(([\d|.]+[e-|\d]*)\*([\d|.]+[e\-|\d]*))",str1).group()
                 num1 = float(equation.split("*")[0])
                 num2 = float(equation.split("*")[1])
                 res = num1*num2
                 res = str(res)
-                str1 = re.sub(r"([\d|.]+\*[\d|.]+)",res,str1,count=1)
+                str1 = re.sub(r"(([\d|.]+[e-|\d]*)\*([\d|.]+[e-|\d]*))",res,str1,count=1)
                 print(str1)
             elif "/" in str1:
-                equation = re.search(r"([\d|.]+/[\d|.]+)",str1).group()
+                equation = re.search(r"(([\d|.]+[e-|\d]*)/([\d|.]+[e-|\d]*))",str1).group()
                 num1 = float(equation.split("/")[0])
                 num2 = float(equation.split("/")[1])
                 res = num1/num2
                 res = str(res)
-                str1 = re.sub(r"([\d|.]+/[\d|.]+)",res,str1,count=1)
+                str1 = re.sub(r"(([\d|.]+[e-|\d]*)/([\d|.]+[e-|\d]*))",res,str1,count=1)
                 print(str1)
-            elif "+" in str1:
-                equation = re.search(r"([\d|.]+\+[\d|.]+)",str1).group()
-                num1 = float(equation.split("+")[0])
-                num2 = float(equation.split("+")[1])
-                res = num1+num2
-                res = str(res)
-                str1 = re.sub(r"([\d|.]+\+[\d|.]+)",res,str1,count=1)
-                print(str1)
-            # elif "-" in str1:
-            #     if re.search(r"e-",str1):
-            #         continue
-            #     else:
-            #         equation = re.search(r"([\d|.]+-[\d|.]+)",str1).group()
-            #         num1 = float(equation.split("-")[0])
-            #         num2 = float(equation.split("-")[1])
-            #         res = num1-num2
-            #         res = str(res)
-            #         str1 = re.sub(r"([\d|.]+-[\d|.]+)",res,str1,count=1)
-            #         print(str1)
             else:
-                exit_flag = True
+                exit_flag  = True
+                return str1
+
+def add_sub(str1):
+    exit_flag = False
+    str1 = re.sub(r"\s","",str1)
+    while not exit_flag:
+        if "+" in str1:
+            equation = re.match(r"(([\d|.]+[e\-|\d]*)\+([\d|.]+[e\-|\d]*))",str1).group()
+            num1 = float(equation.split("+")[0])
+            num2 = float(equation.split("+")[1])
+            res = num1+num2
+            res = str(res)
+            str1 = re.sub(r"(([\d|.]+[e\-|\d]*)\+([\d|.]+[e\-|\d]*))",res,str1,count=1)
+            print(str1)
+        elif "-" in str1:
+            equation = re.match(r"(([\d|.]+[e\-|\d]*)-([\d|.]+[e\-|\d]*))",str1).group()
+            num1 = float(equation.split("-")[0])
+            num2 = float(equation.split("-")[1])
+            res = num1-num2
+            res = str(res)
+            str1 = re.sub(r"(([\d|.]+[e\-|\d]*)-([\d|.]+[e\-|\d]*))",res,str1,count=1)
+            print(str1)
+        else:
+            exit_flag = True
 
 
 
-
-    return str
-
-
-a = mul_div("(9-2*5/3 + 7/3*99/4*2998 +10 * 568/14 )")
+a = mul_div("(-40/5)")
+print(a)
 
 
