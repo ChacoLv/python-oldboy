f7269e8083022e7b31bb5a86a57e205c2514bae8
clv <chenglv@live.com>
2017-07-31 23:10:19 +0800

    file operation

--- day6/cs play.py
+++ day6/cs play.py
@@ -3,16 +3,26 @@
 
 
 class Role:
+    number = 123   #类变量
+    test_list = []
     def __init__(self,name,role,weapon,life_value=100,money=15000):
         #构造函数
         #在实例化时做一些类的初始化工作
-        self.name = name
+        self.name = name            #实例变量（静态属性），作用域就是实例本身
         self.role = role
         self.weapon = weapon
-        self.life_value = life_value
+        self.__life_value = life_value      #私有属性
         self.money = money
+    def show_status(self):
+        print("%s life value is %s"%(self.name,self.__life_value))  #通过调用类的函数进行查看私有属性
+    def __show(self):   #私有方法，加两个_
+        pass
 
-    def shot(self):
+    def __del__(self):
+        #析构函数
+        print("%s is die!!!"%self.name)
+
+    def shot(self):#类的方法、功能，（动态属性）
         print("%s shoting..."%self.name)
 
     def got_shot(self):
@@ -25,6 +35,31 @@ class Role:
         print("%s is buying %s"%(self.name,gun_name))
 #类实例化
 
-r1 = Role("LC","Terrorist","AK47")
+r1 = Role("LC","Terrorist","AK47")          #实例化，生成一个角色，又叫做Role这个类的实例
 r1.shot()
 r1.buy_gun("M4A1")
+r2 = Role("xm","good guy","m4a1")
+#实例变量优先于类变量
+
+r1.bullet_prove = True      #可新增实例变量
+r1.name = "hell"            #修改实例变量
+del r1.name                 #删除实例变量
+
+r1.number = "改类变量"      #改变r1的类变量，只对r1有效
+
+#私有属性查看方法
+r1.show_status()
+
+r1.test_list.append("from r1")
+r2.test_list.append("from r2")
+
+
+print(r1.name,r1.number)
+
+
+
+
+
+
+
+
---
+++ day6/inherit.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+# LC
+
+#class People:  经典类
+#class People(object):  新式类
+
+class People(object):
+
+    def __init__(self,name,age,):
+        self.name = name
+        self.age = age
+
+    def eat(self):
+        print("%s is eating"%self.name)
+
+    def sleep(self):
+        print("%s is sleeping"%self.name)
+
+    def talk(self):
+        print("%s is talking"%self.name)
+
+class Relation(object):
+
+    def make_friends(self,obj):
+        print("%s is trying to make friends with %s"%(self.name,obj.name))
+
+class Man(People,Relation):      #继承父类,多继承
+    def __init__(self,name,age,money):      #对构造函数进行重构
+        People.__init__(self,name,age)      #继承父类的参数继承
+        #super(Man,self).__init__(name,age)  继承父类的构造函数，与People.__init__(self,name,age)一样，新式类写法
+        self.money = money
+        print("%s born with %s money"%(self.name,self.money))
+
+    def piao(self):
+        print("%s is piaoing"%self.name)
+
+    def sleep(self):            #更新，重构父类方法
+        People.sleep(self)
+        print("man sleeping ,update")
+
+class Women(People,Relation):   #多继承，多继承在实例在实例化过程中，查找构造函数的时候，先从本类找，如果没有，再从父类找，多继承的话，从继承的父类按左至右找
+
+    def get_birth(self):
+        print("birth")
+
+m1 = Man("LC",16,100)
+m1.eat()
+
+w1 = Women("Pxm",11)
+
+m1.make_friends(w1)
