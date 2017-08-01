1bc7e2ab6d3dbf060191ec4a629298e25d264a76
clv89 <chenglv@live.com>
2017-08-01 21:14:00 +0800

    20170630

--- day5/calc.py
+++ day5/calc.py
@@ -34,28 +34,40 @@ def add_sub(str1):
     exit_flag = False
     str1 = re.sub(r"\s","",str1)
     while not exit_flag:
-        if "+" in str1:
-            equation = re.match(r"(([\d|.]+[e\-|\d]*)\+([\d|.]+[e\-|\d]*))",str1).group()
-            num1 = float(equation.split("+")[0])
-            num2 = float(equation.split("+")[1])
-            res = num1+num2
-            res = str(res)
-            str1 = re.sub(r"(([\d|.]+[e\-|\d]*)\+([\d|.]+[e\-|\d]*))",res,str1,count=1)
-            print(str1)
-        elif "-" in str1:
-            equation = re.match(r"(([\d|.]+[e\-|\d]*)-([\d|.]+[e\-|\d]*))",str1).group()
-            num1 = float(equation.split("-")[0])
-            num2 = float(equation.split("-")[1])
-            res = num1-num2
-            res = str(res)
-            str1 = re.sub(r"(([\d|.]+[e\-|\d]*)-([\d|.]+[e\-|\d]*))",res,str1,count=1)
-            print(str1)
+        if str1.startswith("-"):
+            num1 = re.match(r"(-[\d|.]*)",str1).group()
+            str1 = re.sub(r"(-[\d|.]*)","",str1,count=1)
+            if str1.startswith("-"):
+                num2 = re.match(r"(-[\d|.]*)",str1).group()
+                res = float(num1) + float(num2)
+                res = str(res)
+                str1 = re.sub(r"(-[\d|.]*)",res,str1,count=1)
+                print(str1)
+            elif str1.startswith("+"):
+                num2 = re.search(r"([\d|.]+)",str1).group()
+                res = float(num1) + float(num2)
+                res = str(res)
+                str1 = re.sub(r"(\+[\d|.]*)",res,str1,count=1)
+            else:
+                res = float(num1)
         else:
-            exit_flag = True
+            num1 = re.match(r"([\d|.]*)",str1).group()
+            str1 = re.sub(r"([\d|.]*)","",str1,count=1)
+            if str1.startswith("-"):
+                num2 = re.match(r"(-[\d|.]*)",str1).group()
+                res = float(num1) + float(num2)
+                res = str(res)
+                str1 = re.sub(r"(-[\d|.]*)",res,str1,count=1)
+            elif str1.startswith("+"):
+                num2 = re.search(r"([\d|.]+)", str1).group()
+                res = float(num1) + float(num2)
+                res = str(res)
+                str1 = re.sub(r"(\+[\d|.]*)",res,str1,count=1)
+            else:
+                res = float(num1)
 
 
-
-a = mul_div("(-40/5)")
+a = add_sub("-3.1-2.3+3.5-1.2")
 print(a)
 
 
