b32562dc263d81876a4c51bc5a45cf3317c9e4e0
clv89 <chenglv@live.com>
2017-08-02 20:35:39 +0800

    20170630

--- day5/calc.py
+++ day5/calc.py
@@ -31,43 +31,73 @@ def mul_div(str1):
                 return str1
 
 def add_sub(str1):
+    #要求输入的字符串必须为不带（）的数值加减，如'-3.1-2.3+3.5-1.2+3-9-9.555+6.555'
+    #逐步取字符串的数字，取两个数字，进行相加，如果有负数or减法，则均为负数，使用加法做运算
+    #取出第一个数字，则将数字从字符串中取出
+    #如果能够取第二个数字，则将两数相加的结果替代
+    #取至最后，替换完后则返回计算结果
     exit_flag = False
     str1 = re.sub(r"\s","",str1)
     while not exit_flag:
         if str1.startswith("-"):
-            num1 = re.match(r"(-[\d|.]*)",str1).group()
-            str1 = re.sub(r"(-[\d|.]*)","",str1,count=1)
+            num1 = re.match(r"(-[\d|.]+[e-|\d]*)",str1).group()
+            str1 = re.sub(r"(-[\d|.]+[e-|\d]*)","",str1,count=1)
             if str1.startswith("-"):
-                num2 = re.match(r"(-[\d|.]*)",str1).group()
+                num2 = re.match(r"(-[\d|.]+[e-|\d]*)",str1).group()
                 res = float(num1) + float(num2)
                 res = str(res)
-                str1 = re.sub(r"(-[\d|.]*)",res,str1,count=1)
-                print(str1)
+                str1 = re.sub(r"(-[\d|.]+[e-|\d]*)",res,str1,count=1)
             elif str1.startswith("+"):
-                num2 = re.search(r"([\d|.]+)",str1).group()
+                num2 = re.search(r"([\d|.]+[e-|\d]*)",str1).group()
                 res = float(num1) + float(num2)
                 res = str(res)
-                str1 = re.sub(r"(\+[\d|.]*)",res,str1,count=1)
+                str1 = re.sub(r"(\+[\d|.]+[e-|\d]*)",res,str1,count=1)
             else:
                 res = float(num1)
+                return res
         else:
-            num1 = re.match(r"([\d|.]*)",str1).group()
-            str1 = re.sub(r"([\d|.]*)","",str1,count=1)
+            num1 = re.match(r"([\d|.]+[e-|\d]*)",str1).group()
+            str1 = re.sub(r"([\d|.]+[e-|\d]*)","",str1,count=1)
             if str1.startswith("-"):
-                num2 = re.match(r"(-[\d|.]*)",str1).group()
+                num2 = re.match(r"(-[\d|.]+[e-|\d]*)",str1).group()
                 res = float(num1) + float(num2)
                 res = str(res)
-                str1 = re.sub(r"(-[\d|.]*)",res,str1,count=1)
+                str1 = re.sub(r"(-[\d|.]+[e-|\d]*)",res,str1,count=1)
             elif str1.startswith("+"):
-                num2 = re.search(r"([\d|.]+)", str1).group()
+                num2 = re.search(r"([\d|.]+[e-|\d]*)", str1).group()
                 res = float(num1) + float(num2)
                 res = str(res)
-                str1 = re.sub(r"(\+[\d|.]*)",res,str1,count=1)
+                str1 = re.sub(r"(\+[\d|.]+[e-|\d]*)",res,str1,count=1)
             else:
                 res = float(num1)
+                return res
 
 
-a = add_sub("-3.1-2.3+3.5-1.2")
+
+
+def re_str(str1):
+    while True:
+        if '--' in str1:
+            str1 = str1.replace("--","+")
+        elif '+-' in str1:
+            str1 = str1.replace("+-","-")
+        elif '-+' in str1:
+            str1 = str1.replace("-+","-")
+        elif '++' in str1:
+            str1 = str1.replace("++","+")
+        else:
+            return str1
+
+
+
+
+
+str1 = '3.1-+2.3++3.5-1.2-+3-9-9.555+6.555'
+
+
+print(str1)
+b = re_str(str1)
+print(b)
+
+a = add_sub(b)
 print(a)
-
-
